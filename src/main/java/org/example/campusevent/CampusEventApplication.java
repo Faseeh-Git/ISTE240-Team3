cdf3bb4e7905329d921a1f5ec95e7712ecc77e7a
Haya Khan <hk4068@rit.edu>
2026-02-18 00:46:21 +0400

    Added initial Spring Boot project structure and html file

---
+++ src/main/java/org/example/campusevent/CampusEventApplication.java
@@ -0,0 +1,13 @@
+package org.example.campusevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusEventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/campusevent/CampusEventApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.campusevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
