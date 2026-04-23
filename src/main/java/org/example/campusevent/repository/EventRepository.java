0936ffe7c5fdbc48678b43c1c6d170bd3035a2c0
BrenZN21 <brendonnoronha21@gmail.com>
2026-04-23 17:07:43 +0400

    Brendon Noronha 761008604

---
+++ src/main/java/org/example/campusevent/repository/EventRepository.java
@@ -0,0 +1,21 @@
+// Name: Brendon
+// ID: 761008604
+
+package org.example.campusevent.repository;
+
+import org.example.campusevent.model.Event;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @Query("SELECT e FROM Event e WHERE e.eventName = :name")
+    List<Event> findByEventName(@Param("name") String name);
+
+    @Modifying
+    @Query("UPDATE Event e SET e.eventLocation = :location WHERE e.eventID = :id")
+    int updateLocationById(@Param("id") Long id,
+                           @Param("location") String location);
+}
