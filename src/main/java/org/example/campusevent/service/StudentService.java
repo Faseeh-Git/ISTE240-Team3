58420c2d9a1359a6e182702648d60fb08e88d8f6
Haya Khan <hk4068@rit.edu>
2026-04-24 15:38:46 +0400

    Add Student service methods for CRUD and search

--- src/main/java/org/example/campusevent/service/StudentService.java
+++ src/main/java/org/example/campusevent/service/StudentService.java
@@ -1,26 +1,56 @@
+// Haya Khan 746008798
 package org.example.campusevent.service;
 
+import jakarta.transaction.Transactional;
 import org.example.campusevent.model.Student;
+import org.example.campusevent.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@Transactional
 public class StudentService {
-    List<Student> students = new ArrayList<Student>();
+    @Autowired
+    private StudentRepository studentRepository;
 
-    public StudentService() {
-        var student1 = new Student(123,"Emma","Emma@gmail.com","CIT");
-        var student2 = new Student(456,"Dan","Dan@gmail.com","CSEC");
-        this.students.add(student1);
-        this.students.add(student2);
+    public List<Student> findAll(){
+        return studentRepository.findAll();
     }
 
-    public List<Student> findAll(){return this.students;}
-
-    public void saveStudent(Student student) {
-        this.students.add(student);
+    public Optional<Student> findById (Long id){
+        return studentRepository.findById(id);
     }
-}
 
+    public List<Student> findByName(String name) {
+        return studentRepository.findByName(name);
+    }
+
+    public List<Student> findByMajor(String major) {
+        return studentRepository.findByMajor(major);
+    }
+
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+
+    public Student updateStudent(Long id, Student updatedStudent) {
+        Student existing = studentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Student not found"));
+        existing.setName(updatedStudent.getName());
+        existing.setEmail(updatedStudent.getEmail());
+        existing.setMajor(updatedStudent.getMajor());
+        return studentRepository.save(existing);
+    }
+
+
+    public void deleteById(Long id) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Delete failed"));
+        studentRepository.delete(student);
+    }
+    }
