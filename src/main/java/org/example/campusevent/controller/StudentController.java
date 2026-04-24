1dabcbb2602cd386d7e94bb3fbebd84260009e01
Haya Khan <hk4068@rit.edu>
2026-04-24 05:40:55 +0400

    Create Student REST controller by adding GET, POST, PUT, DELETE, search

--- src/main/java/org/example/campusevent/controller/StudentController.java
+++ src/main/java/org/example/campusevent/controller/StudentController.java
@@ -1,31 +1,52 @@
+// Haya Khan 746008798
 package org.example.campusevent.controller;
 
 import org.example.campusevent.model.Student;
 import org.example.campusevent.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+
 public class StudentController {
-    private final StudentService studentService;
-    public StudentController(StudentService studentService){this.studentService = studentService;}
+    @Autowired
+    private StudentService studentService;
+//    public StudentController(StudentService studentService){this.studentService = studentService;}
 
-    @GetMapping("/student")
-    public String getStudent(Model data){
-        data.addAttribute("studentList", studentService.findAll());
-        return "student";
+    @GetMapping
+    public List<Student> getAll() {
+        return studentService.findAll();
     }
-    @GetMapping("/student/add")
-    public String showAddStudentForm(Model data){
-        return "add-student";
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getById(@PathVariable Long id) {
+        return studentService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
-    @PostMapping("/student/add")
-    public String saveStudent(Student student){
-        this.studentService.saveStudent(student);
-        return "success";
+    @GetMapping("/search")
+    public List<Student> searchByMajor(@RequestParam String major) {
+        return studentService.findByMajor(major);
+    }
+    @PostMapping
+    public Student create(@RequestBody Student student){
+        return studentService.saveStudent(student);
+    }
+    @PutMapping("/{id}")
+    public Student update(@PathVariable Long id, @RequestBody Student student) {
+        return studentService.updateStudent(id, student);
+    }
+
+    // 6. DELETE (Requirement 3)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        studentService.deleteById(id);
     }
 }
 
